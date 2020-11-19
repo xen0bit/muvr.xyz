ea68fc2d9fb76b79049e2dcde05c3cb572c5bbc4
xen0bit <remaclematt@gmail.com>
2020-11-19 09:42:06 -0500

    revert

--- muvr-dia/build-all.ps1
+++
@@ -1,4 +0,0 @@
-.\build-windows.bat
-wsl bash build-linux.sh
-wsl bash build-macos.sh
-npm run package
--- muvr-dia/buildscripts/build-macos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin
-git pull
-git reset --hard origin/master
-npm run build-macos
