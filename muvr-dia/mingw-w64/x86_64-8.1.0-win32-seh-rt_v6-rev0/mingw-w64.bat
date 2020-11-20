51271d1a3dfb24697826339818b58f12e1e38f28
xen0bit <remaclematt@gmail.com>
2020-11-20 09:38:41 -0500

    mingw-w64

---
+++ muvr-dia/mingw-w64/x86_64-8.1.0-win32-seh-rt_v6-rev0/mingw-w64.bat
@@ -0,0 +1,6 @@
+echo off
+set PATH=C:\Program Files\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin;%PATH%
+rem echo %PATH%
+rem cd "C:\Program Files\mingw-w64\x86_64-8.1.0-win32-seh-rt_v6-rev0\mingw64\bin"
+cd "C:\"
+"C:\WINDOWS\system32\cmd.exe"
